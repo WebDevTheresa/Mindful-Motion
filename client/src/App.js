504de9a9697f51510c22f26e21807af8454c7037
webdevtheresa <webdevtheresa@gmail.com>
2023-10-09 17:18:22 -0400

    needs responsivity

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TeamsPage from './TeamsPage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={'home'} />
-        {/* <Route path="/team" element={team}/>
-        <Route path="/classes" element={classes}/>
-        <Route path="/packages" element={packages}/>
-        <Route path="/schedule" element={contact}/> */}
+        <Route path="/teamspage" element={<TeamsPage />} />
+        {/* <Route path="/classes" element={<classes/>}/> */}
+        {/* <Route path="/packages" element={<packages/>}/> */}
+        {/* <Route path="/schedule" element={<çcontact/>}/>  */}
       </Routes>
     </Router>
   );
