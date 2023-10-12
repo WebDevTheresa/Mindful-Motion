d40ccde3fe2229ae6ed22d979305bddc43cd765c
webdevtheresa <webdevtheresa@gmail.com>
2023-10-11 22:32:27 -0400

    initialize normalize npm

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TeamsPage from './TeamsPage';
 import Fitnessclass from './Fitnessclass';
+import Normalize from 'react-normalize';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={'home'} />
-        <Route path="/teamspage" element={<TeamsPage />} />
-        <Route path="/Fitnessclass" element={<Fitnessclass />} />
-        {/* <Route path="/packages" element={packages}/>
+    <>
+      <Normalize />
+      <Router>
+        <Routes>
+          <Route path="/" element={'home'} />
+          <Route path="/teamspage" element={<TeamsPage />} />
+          <Route path="/Fitnessclass" element={<Fitnessclass />} />
+          {/* <Route path="/packages" element={packages}/>
         <Route path="/schedule" element={contact}/>  */}
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
+    </>
   );
 };
 
