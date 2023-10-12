d018fcedbeb1e407676c69be84fe246796e08ba9
webdevtheresa <webdevtheresa@gmail.com>
2023-10-11 22:31:37 -0400

    fitness class page

--- client/src/Fitnessclass.js
+++ client/src/Fitnessclass.js
@@ -12,87 +12,116 @@ const Fitnessclass = () => {
     <div>
       <Navbar />
       <Header>Fitness Classes</Header>
-      <Bodywrapper>
-        <Wrapper>
-          <Imgdiv src={advancestretch} alt="fitness" />
-          <Infodivodd>
-            <h2> Advanced Pilates</h2>
-            <h3> 45 Minutes</h3>
-            <div>
-              {/* <p> */}
-              Our Pilates class is an abdominal and back-strengthening class
-              that emphasizes strengthening of all the muscles, even the full
-              body. Its roots are in dance, therapy, and yoga. Participants will
-              see an improvement in themselves and their bodies including more
-              flexibility, more strength, better posture, a decrease in neck and
-              back pain, and a less stressed feeling.
-              {/* </p> */}
-            </div>
-          </Infodivodd>
-        </Wrapper>
-        <Wrapper>
-          <Imgdiv src={boxing} alt="fitness" />
-          <Infodiveven>
-            <h2>Boxing</h2> <h3> 60 Minutes</h3>
-            <p>
-              Our boxing class is a high-impact workout that sculpts by fusing
-              authentic martial arts, boxing, and cardiovascular training. This
-              class is designed to improve strength, endurance, and agility
-              while also providing a fun and challenging workout.
-            </p>
-          </Infodiveven>
-        </Wrapper>
-        <Wrapper>
-          <Imgdiv src={yoga} alt="fitness" />
-          <Infodivodd>
-            <h2>Yoga</h2> <h3>45 Minutes</h3>{' '}
-            <p>
-              Our yoga class is a mind/body exercise program that will deliver a
-              full-body workout with an emphasis on yoga poses and core
-              strengthening. Benefits include increased strength, flexibility,
-              and balance, as well as reduced stress and improved mental
-              clarity.
-            </p>
-            <p>*Please bring your yoga mat for this class</p>
-          </Infodivodd>
-        </Wrapper>
-        <Wrapper>
-          <Imgdiv src={stretching} atl="fitness" />
-          <Infodiveven>
-            <h2>Advance stretching</h2> <h3>40 Minutes</h3>
-            <p>
-              Our stretching class is designed to improve flexibility and
-              mobility. We use a variety of stretching techniques, including
-              static stretching, dynamic stretching, and foam rolling, to help
-              our clients achieve their goals.
-            </p>
-          </Infodiveven>
-        </Wrapper>
-        <Wrapper>
-          <Imgdiv src={barre} alt="fitness" />
-          <Infodivodd>
-            <h2>Barre Pilates</h2> <h3>45 minutes</h3>
-            <p>
-              Our Barre Pilates class combines the principles of Pilates with
-              ballet-inspired movements. This class is low impact and focuses on
-              toning, sculpting, and strengthening the body, while also
-              improving posture and balance.
-            </p>
-          </Infodivodd>
-        </Wrapper>
-        <Wrapper>
-          <Imgdiv src={pilates} alt="fitness" />
-          <Infodiveven>
-            <h2>Beginner Pilates</h2> <h3>40 Minutes</h3>
-            <p>
-              This class is designed for individuals who are new to Pilates or
-              who want to refresh their knowledge of the basics. Participants
-              will learn proper breathing techniques, alignment, and form while
-              performing exercises that target the core, arms, legs, and back.
-            </p>
-          </Infodiveven>
-        </Wrapper>
-      </Bodywrapper>
+
+      <Wrapper>
+        <Img src={advancestretch} alt="fitness" />
+        <Infodivodd>
+          <Title>
+            <Title>
+              <H2> Advanced Pilates</H2>
+              <h3> 45 Minutes</h3>
+            </Title>
+            Our Pilates class is an abdominal and back-strengthening class that
+            emphasizes strengthening of all the muscles, even the full body. Its
+            roots are in dance, therapy, and yoga. Participants will see an
+            improvement in themselves and their bodies including more
+            flexibility, more strength, better posture, a decrease in neck and
+            back pain, and a less stressed feeling.
+            <Span>
+              Best For:{' '}
+              <span>People who have taken several regular Pilates classes</span>
+            </Span>
+          </Title>
+        </Infodivodd>
+      </Wrapper>
+      <Wrapper>
+        <Infodiveven>
+          <Title>
+            <Title>
+              <H2>Boxing</H2> <h3> 60 Minutes</h3>
+            </Title>
+            Our boxing class is a high-impact workout that sculpts by fusing
+            authentic martial arts, boxing, and cardiovascular training. This
+            class is designed to improve strength, endurance, and agility while
+            also providing a fun and challenging workout.
+            <Span>
+              Best For: <span>Everyone!</span>
+            </Span>
+          </Title>
+          <Img src={boxing} alt="fitness" />
+        </Infodiveven>
+      </Wrapper>
+      <Wrapper>
+        <Img src={yoga} alt="fitness" />
+        <Infodivodd>
+          <Title>
+            <Title>
+              <H2>Yoga</H2> <h3>45 Minutes</h3>{' '}
+            </Title>
+            Our yoga class is a mind/body exercise program that will deliver a
+            full-body workout with an emphasis on yoga poses and core
+            strengthening. Benefits include increased strength, flexibility, and
+            balance, as well as reduced stress and improved mental clarity.
+            <Span>
+              Best For: <span>Yoga lovers</span>
+            </Span>
+            <Title>*Please bring your yoga mat for this class</Title>
+          </Title>
+        </Infodivodd>
+      </Wrapper>
+      <Wrapper>
+        <Infodiveven>
+          <Title>
+            <Title>
+              <H2>Advance stretching</H2> <h3>40 Minutes</h3>
+            </Title>
+            Our stretching class is designed to improve flexibility and
+            mobility. We use a variety of stretching techniques, including
+            static stretching, dynamic stretching, and foam rolling, to help our
+            clients achieve their goals.
+            <Span>
+              Best For:
+              <span>people, who is looking to increase flexibility</span>
+            </Span>{' '}
+          </Title>
+          <Img src={stretching} atl="fitness" />
+        </Infodiveven>
+      </Wrapper>
+      <Wrapper>
+        <Img src={barre} alt="fitness" />
+        <Infodivodd>
+          <Title>
+            <Title>
+              <H2>Barre Pilates</H2> <h3>45 minutes</h3>
+            </Title>
+            Our Barre Pilates class combines the principles of Pilates with
+            ballet-inspired movements. This class is low impact and focuses on
+            toning, sculpting, and strengthening the body, while also improving
+            posture and balance.
+            <Span>
+              Best For: <span>Barre lovers and low impact enthusiasts</span>
+            </Span>
+          </Title>
+        </Infodivodd>
+      </Wrapper>
+      <Wrapper>
+        <Infodiveven>
+          <Title>
+            <Title>
+              <H2>Beginner Pilates</H2> <h3>40 Minutes</h3>
+            </Title>
+            This class is designed for individuals who are new to Pilates or who
+            want to refresh their knowledge of the basics. Participants will
+            learn proper breathing techniques, alignment, and form while
+            performing exercises that target the core, arms, legs, and back.
+            <Span>
+              Best For:{' '}
+              <span>Anyone looking to start their pilates journey</span>
+            </Span>{' '}
+          </Title>
+          <Img src={pilates} alt="fitness" />
+        </Infodiveven>
+      </Wrapper>
     </div>
   );
 };
@@ -107,38 +136,85 @@ const Header = styled.div`
   font-size: 80px;
   color: white;
   align-items: flex-end;
-`;
-const Bodywrapper = styled.div`   
 
-const Imgdiv = styled.img`; // height:400px; // width:350px; // `
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    padding: 40px 40px;
+    width: 100%;
+  }
+`;
+
+const Span = styled.span`
+  font-family: 'Roboto', sans-serif;
+  font-family: 'Ubuntu', sans-serif;
+  font-weight: bold;
+`;
+const H2 = styled.h2`
+  font-size: 45px;
+  font-family: 'Roboto', sans-serif;
+  font-family: 'Ubuntu', sans-serif;
+`;
+const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: 'Roboto', sans-serif;
+  font-family: 'Ubuntu', sans-serif;
+`;
 
 const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
   margin: 50px;
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    // padding: 40px 40px;
+    width: 100%;
+  }
 `;
 const Infodivodd = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: flex-start;
-  width: 500px;
+  width: 100%;
+  margin: 50px;
+  line-height: 2em;
   font-family: 'Roboto', sans-serif;
   font-family: 'Ubuntu', sans-serif;
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    // padding: 40px 40px;
+    width: 100%;
+  }
 `;
 
 const Infodiveven = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
-  width: 500px;
+  line-height: 2em;
+  margin: 50px;
+  width: 100%;
   font-family: 'Roboto', sans-serif;
   font-family: 'Ubuntu', sans-serif;
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    // padding: 40px 40px;
+    width: 100%;
+  }
 `;
 
-const Imgdiv = styled.img`
-  height: 25em;
+const Img = styled.img`
+  width: 296px;
+  height: 373px;
 
-  //   width: 100%;
-  //   height: auto;
-  //   max-width: 100%;
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    width: 100%;
+    // height: 66%;
+  }
 `;
 
 export default Fitnessclass;
