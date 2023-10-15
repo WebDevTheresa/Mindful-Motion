88aa29aba9b3cb43638ab20ab79b4a645cb7bd84
webdevtheresa <webdevtheresa@gmail.com>
2023-10-15 13:22:57 -0400

    route added

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TeamsPage from './TeamsPage';
 import Fitnessclass from './Fitnessclass';
 import Normalize from 'react-normalize';
-
+import Reformerclass from './Reformerclass';
 const App = () => {
   return (
     <>
@@ -13,6 +13,7 @@ const App = () => {
           <Route path="/" element={'home'} />
           <Route path="/teamspage" element={<TeamsPage />} />
           <Route path="/Fitnessclass" element={<Fitnessclass />} />
+          <Route path="/Reformerclass" element={<Reformerclass />} />
           {/* <Route path="/packages" element={packages}/>
         <Route path="/schedule" element={contact}/>  */}
         </Routes>
