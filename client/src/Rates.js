f84fb3289f9b5ac780200ef72bc4fbc5b8422183
webdevtheresa <webdevtheresa@gmail.com>
2023-10-15 13:33:00 -0400

    component created

---
+++ client/src/Rates.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import Navbar from './NavBar';
+import Footer from './Footer';
+import IntroOffer from './IntroOffer';
+
+const Rates = () => {
+  return (
+    <>
+      <Navbar />
+      <Header>Prices</Header>
+      <IntroOffer />
+      <Footer />
+    </>
+  );
+};
+
+const Header = styled.div`
+  height: 170px;
+  width: 100%;
+  padding: 20px;
+  background-color: #84a98c;
+  display: flex;
+  justify-content: flex-start;
+  font-size: 80px;
+  color: white;
+  align-items: flex-end;
+
+  @media only screen and (max-width: 768px) {
+    // flex-direction: column;
+    // padding: 40px 40px;
+    height: 72px;
+    width: 100%;
+    display: block;
+  }
+`;
+export default Rates;
