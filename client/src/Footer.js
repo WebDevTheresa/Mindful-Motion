714bfcf7a2dc240259adc29f8f861428b1f230b8
webdevtheresa <webdevtheresa@gmail.com>
2023-10-14 21:11:43 -0400

    component created

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -7,40 +7,77 @@ const Footer = () => {
   return (
     <>
       <MainWrapper>
-        <Wrapper>
+        <GeneralWrapper>
           <img src={logo} alt="logo" style={{ width: '4em', height: '4em' }} />
-          <p>IA, 2000 McGill College Avenue Tower Montreal QC H3A 3H3</p>
-        </Wrapper>
-        <Wrapper>
-          <h6>Classes</h6>
+          <p>
+            IA, 2000 McGill College Avenue Tower <br></br> Montreal QC H3A 3H3
+          </p>
+        </GeneralWrapper>
+        <ClassWrapper>
+          <h2>Classes</h2>
           <p>Advanced Pilates</p>
           <p>Boxing</p>
           <p>Yoga</p>
           <p>Advanced Stretching</p>
           <p>Barre Pilates</p>
           <p>Beginner Pilates</p>
-        </Wrapper>
-        <Wrapper>
-          <h6>General</h6>
+        </ClassWrapper>
+        <ContactWrapper>
+          <h2>General</h2>
           <p>Team</p>
           <p>Schedule</p>
           <p>Rates & Packages</p>
           <p>Contact</p>
-          <FacebookIcon style={{ width: '2em', height: '2em' }} />
-          <InstagramIcon style={{ width: '2em', height: '2em' }} />
-        </Wrapper>
+          <SvgWrapper>
+            <FacebookIcon style={{ width: '2em', height: '2em' }} />
+            <InstagramIcon style={{ width: '2em', height: '2em' }} />
+          </SvgWrapper>
+        </ContactWrapper>
       </MainWrapper>
     </>
   );
 };
 
-const Wrapper = styled.div`
-  display: flex;
-  //   flex-direction: column;
-  justify-content: space-around;
-`;
-
 const MainWrapper = styled.div`
   display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: flex-start;
+  margin-top: 16px;
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    padding: 20px;
+  }
+`;
+
+const GeneralWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media only screen and (max-width: 768px) {
+    margin-bottom: 20px;
+  }
+`;
+const ClassWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media only screen and (max-width: 768px) {
+    margin-bottom: 20px;
+  }
+`;
+const SvgWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  @media only screen and (max-width: 768px) {
+    margin-top: 20px;
+  }
+`;
+const ContactWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media only screen and (max-width: 768px) {
+    margin-bottom: 20px;
+  }
 `;
 export default Footer;
